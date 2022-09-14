fb435878007060f118ac99c8fc779dd04530146c
Surovtsev Vladimir <surovtsev@MacBook-Air-Technical-Department.local>
2022-09-14 21:09:53 +0300

    if else constr

---
+++ ifelse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+   Console.WriteLine("Ура, рад тебя видеть");   
+}
+else
+{
+   Console.Write("привет,");
+   Console.WriteLine(username);
+}
+
